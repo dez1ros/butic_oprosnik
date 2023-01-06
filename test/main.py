1d5cbf514eb37f62457c8f853df0ba122d70d4d8
dez1ros <aleksandrv2007@gmail.com>
2023-01-06 14:44:41 +0300

    Админка фулл вроде как готова

--- test/main.py
+++ test/main.py
@@ -16,8 +16,8 @@ cur.execute("""CREATE TABLE IF NOT EXISTS users(
 """)  # создание бд с логином и паролем админа
 
 cur.execute("""CREATE TABLE IF NOT EXISTS photos(
-   user TEXT,
-   photo BLOB);
+   name TEXT,
+   photo TEXT);
 """)  # бд с фотками
 
 if cur.execute("SELECT * FROM users").fetchone() is None:  # Добавляем логин и пароль администратора если его нет
@@ -29,11 +29,6 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SECRET_KEY'] = '8614b78a4b9c76bac8fdab1e5792ffb47ce9d66e'  # шифр сессии
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-def allowed_file(filename):  # проверка подходит ли нам выгруженный пользователем файл
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
@@ -70,35 +65,60 @@ def adm_reg():
 
 @app.route('/adm_panel', methods=['POST', 'GET'])  # админ панель
 def adm_panel():
-    global LAST_FILE
     if 'login' not in session:
         return redirect('/admin')
     if request.method == "POST":
         file = request.files['photo']
         # print(os.path.exists(UPLOAD_FOLDER + "\\" + file.filename))           проверка есть ли файл
         photos = cur.execute("SELECT photo FROM photos").fetchall()
-        if file and allowed_file(file.filename):
-            if photos:
-                photos = photos[-1][0]
-                print(photos, int(photos.split('.')[0]))
-                cur.execute(f"""INSERT INTO photos(user, photo)
-                           VALUES('{request.form['user']}', '{int(photos.split('.')[0]) + 1}.jpg');""")
-                filename = secure_filename(f"{int(photos.split('.')[0]) + 1}.jpg")
-            else:
-                cur.execute(f"""INSERT INTO photos(user, photo)
-                                           VALUES('{request.form['user']}', '{1}.jpg');""")
-                filename = secure_filename(f"{1}.jpg")
+        if photos:
+            photos = photos[-1][0]
+            print(photos, int(photos.split('.')[0]))
+            cur.execute(f"""INSERT INTO photos(name, photo)
+                           VALUES('{request.form['name']}', '{int(photos.split('.')[0]) + 1}.jpg');""")
+            filename = secure_filename(f"{int(photos.split('.')[0]) + 1}.jpg")
+        else:
+            cur.execute(f"""INSERT INTO photos(name, photo)
+                                           VALUES('{request.form['name']}', '{1}.jpg');""")
+            filename = secure_filename(f"{1}.jpg")
 
-            conn.commit()
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        conn.commit()
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect('/adm_panel')
 
-    names = cur.execute("SELECT user FROM photos").fetchall()
+    names = cur.execute("SELECT name FROM photos").fetchall()
     photos = cur.execute("SELECT photo FROM photos").fetchall()
     print(names, photos)
     return render_template('adm_panel.html', names=names, photos=photos, len=len(names))
 
 
-@app.route('/exit')
+@app.route('/edit', methods=['POST', 'GET'])  # поменять что нить
+def edit():
+    global filename
+    if 'login' not in session:
+        return redirect('/admin')
+    if request.method == 'GET':
+        filename = request.args.get('image')
+    if request.method == 'POST':
+        cur.execute(f"UPDATE photos SET name='{request.form['name']}' WHERE photo='{filename}'")
+        conn.commit()
+        file = request.files['photo']
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect('/adm_panel')
+    name = cur.execute(f"SELECT name FROM photos WHERE photo='{filename}'").fetchone()[0]
+    return render_template('edit.html', image=filename, name=name)
+
+
+@app.route('/del/<filename>')
+def delit(filename):
+    if 'login' not in session:
+        return redirect('/admin')
+    cur.execute(f"DELETE FROM photos WHERE photo='{filename}'")
+    conn.commit()
+    return redirect('/adm_panel')
+
+
+@app.route('/exit')  # выход из аккаунта
 def ex():
     session.clear()
     return redirect('/')
